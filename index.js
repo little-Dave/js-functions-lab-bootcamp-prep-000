12481c47f579e5b27eb5fa34b7b3b83cec4209ce
Dave Boehmer <daboehmer@gmail.com>
2019-04-01 00:12:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 
+function happyHolidaysTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(days, holiday) {
+  return `It's ${days} until ${holiday}!`
+}
